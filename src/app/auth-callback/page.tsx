cdcfd252d9906d5a9478467b81dd9391bad6ffdc
kevinQTruong <kevinQTruong@users.noreply.github.com>
2023-10-01 13:39:39 +1100

    add trpc setup with authCallback

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,22 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+
+const AuthCBPage = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const origin = searchParams.get('origin')
+
+
+  const { data, } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        // user is synced to db
+        router.push(origin ? `/${origin}` : '/dashboard')
+      }
+
+    }
+  })
+
+}
+
+export default AuthCBPage;

7aafc999e996e73fc69eff59837680915ae828a9
kevinQTruong <kevinQTruong@users.noreply.github.com>
2023-11-05 11:27:49 +1100

    add pinecone index upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,10 @@
 import { db } from "@/db"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { createUploadthing, type FileRouter } from "uploadthing/next"
+import { PDFLoader } from "langchain/document_loaders/fs/pdf"
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"
+import { PineconeStore } from "langchain/vectorstores/pinecone"
+import { pinecone } from "@/lib/pinecone"
 
 const f = createUploadthing()
 
@@ -28,6 +32,39 @@ export const ourFileRouter = {
           uploadStatus: "PROCESSING",
         },
       })
+
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        )
+
+        const blob = await response.blob()
+        const loader = new PDFLoader(blob)
+        const pageLevelDocs = await loader.load()
+        const pageAmt = pageLevelDocs.length
+
+        // vectorize and index entire document
+        const pineconeIndex = pinecone.Index("yolo")
+
+        await PineconeStore.fromDocuments(
+          pageLevelDocs,
+          new OpenAIEmbeddings(),
+          {
+            pineconeIndex,
+            // namespace: createdFile.id, // adding this break the storing on pinecone. try another service later
+          }
+        )
+
+        await db.file.update({
+          data: { uploadStatus: "SUCCESS" },
+          where: { id: createdFile.id },
+        })
+      } catch (error) {
+        await db.file.update({
+          data: { uploadStatus: "FAILED" },
+          where: { id: createdFile.id },
+        })
+      }
     }),
 } satisfies FileRouter
 

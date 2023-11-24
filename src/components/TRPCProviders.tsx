c5b8bc951306db3d6b14ed6c6313d4d36c3240fe
kevinQTruong <kevinQTruong@users.noreply.github.com>
2023-11-25 10:09:44 +1100

    fix url issue for prod

--- src/components/TRPCProviders.tsx
+++ src/components/TRPCProviders.tsx
@@ -4,11 +4,12 @@ import { PropsWithChildren, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/client";
+import { absoluteUrl } from "@/lib/utils";
 
 const TRPCProvider = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient())
   const [trpcClient] = useState(() => trpc.createClient({
-    links: [httpBatchLink({ url: 'http://localhost:3000/api/trpc' })]
+    links: [httpBatchLink({ url: absoluteUrl("/api/trpc") })]
   }))
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>

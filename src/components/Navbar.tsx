c118059c9c6c45628bbcfa3f667b31ad80dfc73f
kevinQTruong <kevinQTruong@users.noreply.github.com>
2023-11-26 08:48:53 +1100

    add mobileNav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { buttonVariants } from './ui/button'
 import { LoginLink, RegisterLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { ArrowRight } from 'lucide-react'
 import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   const { getUser } = getKindeServerSession()
@@ -20,6 +21,8 @@ const Navbar = () => {
           <Link href='/' className='flex z-40 font-semibold'>
             <span>quill.</span>
           </Link>
+
+          <MobileNav isAuth={!!user} />
 
           <div className='hidden items-center space-x-4 sm:flex'>
             {!user ? (

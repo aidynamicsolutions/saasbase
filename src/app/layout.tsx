db9c7532fc3accda0ec76c49ed418155286a6b18
kevinQTruong <kevinQTruong@users.noreply.github.com>
2023-12-18 21:59:37 +1100

    upgrade to next 14

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
-import { cn, constructMetadata } from '@/lib/utils'
 import './globals.css'
 import "react-loading-skeleton/dist/skeleton.css"
 import "simplebar-react/dist/simplebar.min.css"
 
+import { cn, constructMetadata } from '@/lib/utils'
+import { Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import TRPCProvider from '@/components/TRPCProviders'
 import { Toaster } from '@/components/ui/toaster'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = constructMetadata()
+
+
+export const viewport: Viewport = {
+  themeColor: '#FFF',
+}
 
 export default function RootLayout({
   children,

05e970897006f8faaed029899aeb2f68a5bec4f4
kevinQTruong <kevinQTruong@users.noreply.github.com>
2024-01-06 10:14:17 +1100

    add logout & user button

---
+++ src/components/auth/UserButton.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { LogOutIcon, User } from "lucide-react"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from "@/components/ui/avatar";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+import { LogoutButton } from "@/components/auth/LogoutButton";
+
+export const UserButton = () => {
+  const user = useCurrentUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || ""} />
+          <AvatarFallback className="bg-sky-500">
+            <User className="text-white" />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="end">
+        <LogoutButton>
+          <DropdownMenuItem>
+            <LogOutIcon className="h-4 w-4 mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};

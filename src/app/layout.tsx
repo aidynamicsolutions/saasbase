609170e9852ced3e9489d7fe59bcf0dbf7fd87ed
kevinQTruong <kevinQTruong@users.noreply.github.com>
2024-01-06 10:13:02 +1100

    add auth session provider to global app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import { Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import TRPCProvider from '@/components/TRPCProviders'
+import { SessionProvider } from 'next-auth/react'
+import { auth } from "@/auth"
 import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,20 +19,23 @@ export const viewport: Viewport = {
   themeColor: '#FFF',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await auth()
   return (
     <html lang="en" className='light'>
-      <TRPCProvider>
-        <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
-          <Toaster />
-          <Navbar />
-          {children}
-        </body>
-      </TRPCProvider>
+      <SessionProvider session={session}>
+        <TRPCProvider>
+          <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+            <Toaster />
+            <Navbar />
+            {children}
+          </body>
+        </TRPCProvider>
+      </SessionProvider>
     </html>
   )
 }
